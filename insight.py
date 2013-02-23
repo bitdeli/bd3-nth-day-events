8beca78f394ef64a4d2305eca2455fc383cf98c1
Ville Tuulos <team+foobar@bitdeli.com>
2013-02-23 02:22:42 +0000

    make basic functionality work

--- insight.py
+++ insight.py
@@ -1,14 +1,36 @@
+from itertools import groupby
 from bitdeli.insight import insight
-from bitdeli.widgets import Text, Bar, Table
-from discodb.query import Q, Literal, Clause
+from bitdeli.widgets import Table
+
+BINS = ['x 01', 'x 02-03', 'x 04-07', 'x 08-15', 'x 16-31', 'x 32-63', 'x 64-']
+
+def keys(model, days):
+    def items():
+        for key in model:
+            day, bin, event = key.split(':', 2)
+            day = int(day)
+            if day in days:
+                yield day, event, int(bin), len(model[key])
+    return list(sorted(items()))
+                
+def make_day(day_data):
+    def scored():
+        for event, items in groupby(day_data, lambda x: x[1]):
+            row = dict((bin, 0) for bin in BINS)
+            row.update((BINS[bin], num_users)
+                       for day, event, bin, num_users in items)
+            row['x total'] = sum(row.itervalues())
+            row['event'] = event
+            yield row
+    return list(sorted(scored(),
+                       key=lambda x: x['x total'],
+                       reverse=True))
 
 @insight
 def view(model, params):
-    params = {'events': ['$signup', 'Clip created']}
-    def steps(events):
-        for i in range(len(events)):
-            q = Q([Clause([Literal(event)]) for event in events[:i + 1]])
-            yield events[i], len(model.query(q))
-    return [Bar(size=(12, 6),
-                data=list(steps(params['events'])))]
+    days = [0, 1, 2]
+    for day, day_data in groupby(keys(model, days), lambda x: x[0]):
+        yield Table(size=(12, 6),
+                    data=make_day(day_data),
+                    label='day %s' % day)
     
--- model.py
+++ model.py
@@ -1,7 +1,36 @@
+from itertools import islice, groupby
+from datetime import datetime
+import math
 from bitdeli.model import model
 
+
+NUM_DAYS = 30
+MAX_BINS = 6
+
+def events(profile):
+    
+    def hour2day(event):
+        return datetime.utcfromtimestamp(event[0] * 3600).toordinal()
+    
+    def hour_lists():
+        for event, hours in profile['events'].iteritems():
+            hour_list = list(hours)
+            yield hour2day(hour_list[-1]), event, hour_list
+    
+    event_list = list(hour_lists())
+    first_day = min(event_list)[0]
+    for _first, event, hour_list in event_list:
+        for day, hours in groupby(reversed(hour_list), hour2day):
+            relative = day - first_day
+            if relative > NUM_DAYS:
+                break
+            else:
+                daily_total = sum(count for hour, count in hours) 
+                yield event, relative, daily_total
+                
 @model
 def build(profiles):
     for profile in profiles:
-        for event in profile['events']:
-            yield event, profile.uid
+        for event, day, count in events(profile):
+            bin = min(int(math.log(count, 2)), MAX_BINS)
+            yield '%s:%s:%s' % (day, bin, event), profile.uid
