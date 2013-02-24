8ea83f81c06c850b9f8913da752797900034037e
Ville Tuulos <team+foobar@bitdeli.com>
2013-02-24 23:03:37 +0000

    color cells

--- insight.py
+++ insight.py
@@ -1,4 +1,5 @@
 from itertools import groupby
+from collections import Counter
 from bitdeli.insight import insight
 from bitdeli.widgets import Table, Widget
 
@@ -9,9 +10,11 @@ class TokenInput(Widget):
     pass
 
 BINS = ['1', '2-3', '4-7', '8-15', '16-31', '32-63', '64-']
-COLUMNS = [{'name': name, 'label': name.capitalize()}
-           for name in ['event'] + BINS + ['total']]
-
+COLUMNS = [{'name': 'event', 'label': 'Event'}]
+COLUMNS += [{'name': bin, 'label': bin, 'cell': 'colorstring'}
+            for bin in BINS]
+COLUMNS += [{'name': 'total', 'label': 'Total'}]
+          
 def keys(model, days):
     def items():
         for key in model:
@@ -24,10 +27,13 @@ def keys(model, days):
 def make_day(day_data):
     def scored():
         for event, items in groupby(day_data, lambda x: x[1]):
-            row = dict((bin, 0) for bin in BINS)
-            row.update((BINS[bin], num_users)
-                       for day, event, bin, num_users in items)
-            row['total'] = sum(row.itervalues())
+            bins = [0] * len(BINS)
+            for day, event, bin, num_users in items:
+                bins[bin] = num_users
+            total = float(sum(bins))
+            row = dict((BINS[i], {'label': bin, 'fill': bin / total})
+                       for i, bin in enumerate(bins))
+            row['total'] = int(total)
             row['event'] = event
             yield row
     return list(sorted(scored(),
